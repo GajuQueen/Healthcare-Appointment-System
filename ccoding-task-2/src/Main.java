551bdafba6bfe72197fdca4d55d2a72627fb1e75
GajuQueen <gajuq7@gmail.com>
2025-04-10 11:31:26 +0200

    add assignment 2

---
+++ ccoding-task-2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int celsius = 25;
+        int fahrenheit = (celsius* (9/5) + 32);
+        System.out.println("25°C is " + fahrenheit + "°F" );
+    }
+}

df1e62e23e2a656e089683367fd3d7b51da55ec6
GajuQueen <gajuq7@gmail.com>
2025-05-01 11:02:27 +0200

    add pair class

---
+++ Key-Value Utility/src/Pair.java
@@ -0,0 +1,29 @@
+public class Pair <k, v>{
+    k key;
+    v value;
+    public Pair(k key, v value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public k getKey() {
+        return key;
+    }
+
+    public void setKey(k key) {
+        this.key = key;
+    }
+
+    public v getValue() {
+        return value;
+    }
+
+    public void setValue(v value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [key=" + key + ", value=" + value + "]";
+    }
+}

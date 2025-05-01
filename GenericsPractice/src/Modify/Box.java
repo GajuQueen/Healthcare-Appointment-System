5fef5409a4d2e5d1bc735f4ee431fe82193e798a
GajuQueen <gajuq7@gmail.com>
2025-05-01 07:47:35 +0200

    Modify(Generics)

--- GenericsPractice/src/Modify/Box.java
+++ GenericsPractice/src/Modify/Box.java
@@ -1,3 +1,5 @@
+package Modify;
+
 public class Box<T> {
     T content;
 
--- GenericsPractice/src/Modify/Main.java
+++ GenericsPractice/src/Modify/Main.java
@@ -1,3 +1,5 @@
+import Modify.Box;
+
 public class Main {
     public static void main(String[] args) {
         Box<String> string = new Box<>();

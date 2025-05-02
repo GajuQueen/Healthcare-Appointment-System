0ee3ad280586ca9304ac8eddcfbf785b4cc7c41d
GajuQueen <gajuq7@gmail.com>
2025-05-02 06:42:35 +0200

    add Lamda expression

--- Key-Value Utility/src/Pair.java
+++ Key-Value Utility/src/Pair.java
@@ -1,7 +1,12 @@
-public class Pair <k, v>{
+import java.util.Objects;
+public class Pair <k, v> {
     k key;
     v value;
+
     public Pair(k key, v value) {
+        if (key == null) {
+            throw new NullPointerException("key is null");
+        }
         this.key = key;
         this.value = value;
     }
@@ -11,6 +16,9 @@ public class Pair <k, v>{
     }
 
     public void setKey(k key) {
+        if (key == null) {
+            throw new NullPointerException("key is null");
+        }
         this.key = key;
     }
 
@@ -24,6 +32,23 @@ public class Pair <k, v>{
 
     @Override
     public String toString() {
-        return   key + ", value= " + value ;
+        return key + ", value= " + value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair<?, ?> other)) return false;
+        return Objects.equals(key, other.key) && Objects.equals(value, other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    Pair<String, Integer> pair = Pair.create("Name", 30);
+    public static <K, V> Pair<K, V> create(K key, V value) {
+        return new Pair<>(key, value);
     }
 }

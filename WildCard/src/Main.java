f809f3f552439913fa85831795d0beb54c25712e
GajuQueen <gajuq7@gmail.com>
2025-05-02 10:29:20 +0200

    add GenericArrayUtility class

--- WildCard/src/Main.java
+++ WildCard/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        NumberPair<Integer, Double> pair1 = new NumberPair<>(5, 4.5);
+        System.out.println("Pair: " + pair1);
+        System.out.println("Sum: " + pair1.sum(2, 5));
+        System.out.println("Product: " + pair1.product());
+
+        NumberPair<Float, Long> pair2 = new NumberPair<>(3.5f, 6L);
+        System.out.println("Pair: " + pair2);
+        System.out.println("Sum: " + pair2.sum());
+        System.out.println("Product: " + pair2.product())
+
     }
 }

1195c65b37ea9344fab2c0d01b50b28471aa87f1
GajuQueen <gajuq7@gmail.com>
2025-05-07 16:10:47 +0200

    add counter

---
+++ ThreadPractice/src/Counter.java
@@ -0,0 +1,12 @@
+public class Counter {
+    private static int count = 0;
+
+    public static void increment() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+}

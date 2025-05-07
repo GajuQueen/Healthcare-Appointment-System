1afb914c918a4b593dbaa4b33c3abb17c6667e59
GajuQueen <gajuq7@gmail.com>
2025-05-07 07:31:47 +0200

    add main

--- Lambda Expression for Filtering Even Numbers/src/Main.java
+++ Lambda Expression for Filtering Even Numbers/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+                List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+                List<Integer> evens1 = EvenNumberFilter.filterEvenNumbers(list1);
+                System.out.println("Original List: " + list1);
+                System.out.println("Even Numbers List: " + evens1);
+                System.out.println();
+
+                List<Integer> list2 = Collections.emptyList();
+                List<Integer> evens2 = EvenNumberFilter.filterEvenNumbers(list2);
+                System.out.println("Original List: " + list2);
+                System.out.println("Even Numbers List: " + evens2);
+                System.out.println();
+
+                List<Integer> list3 = Arrays.asList(1, 3, 5, 7, 9);
+                List<Integer> evens3 = EvenNumberFilter.filterEvenNumbers(list3);
+                System.out.println("Original List: " + list3);
+                System.out.println("Even Numbers List: " + evens3);
+            }
+        }

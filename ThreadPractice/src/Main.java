3b802d2defb560297a282e551e0c90d242c5da44
GajuQueen <gajuq7@gmail.com>
2025-05-07 11:10:08 +0200

    add main

--- ThreadPractice/src/Main.java
+++ ThreadPractice/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+     t1.setName("Gaju");
+     t1.start();
+        t1.setName("Queen");
+        t1.start();
     }
 }
--- ThreadPractice/src/MyThread.java
+++ ThreadPractice/src/MyThread.java
@@ -2,7 +2,7 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Gaju" + i);
+            System.out.println(getName() + i);
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {

8e55a29e33eef952674259d053468e7f8986304b
GajuQueen <gajuq7@gmail.com>
2025-05-28 13:17:01 +0200

    add commenetcontroller

---
+++ BlogPlatforms/src/main/java/org/example/blogplatforms/Comment/CommentController.java
@@ -0,0 +1,27 @@
+package org.example.blogplatforms.Comment;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Posts")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    @Operation(summary = "Add a comment to a Post")
+    public Comment addcomment(@RequestBody Comment comment) {
+        return commentService.addcomment(comment);
+    }
+
+    @GetMapping
+    @Operation
+    public List<Comment> getallcomments(@RequestBody Comment comment) {
+        return commentService.getallcomments();
+    }
+
+}

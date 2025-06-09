8ee5b31df30c3bf887a3b5ffdf04086e65346960
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-06-09 13:00:27 +0200

    feat: Implement business logic for Clinic management

--- src/main/java/com/example/demo/Clinic/ClinicService.java
+++ src/main/java/com/example/demo/Clinic/ClinicService.java
@@ -1,4 +1,44 @@
 package com.example.demo.Clinic;
 
+import com.example.demo.Dto.ClinicDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ClinicService {
+    private final ClinicRepository clinicRepository;
+
+    public Clinic createClinic(ClinicDto dto) {
+        Clinic clinic = new Clinic();
+
+        clinic.setName(dto.getName());
+        clinic.setAddress(dto.getAddress());
+        clinic.setPhone(dto.getPhone());
+        return clinicRepository.save(clinic);
+    }
+
+    public List<Clinic> getAllClinics() {
+        return clinicRepository.findAll();
+    }
+    public Clinic getClinicById(int id){
+        return clinicRepository.findById(id).orElseThrow(() -> new RuntimeException("Clinic not Found."));
+    }
+
+    public Clinic updateClinic(int id,ClinicDto dto){
+
+        Clinic clinic = clinicRepository.findById(id).orElseThrow(() -> new RuntimeException("clinic not found."));
+        clinic.setName(dto.getName());
+        clinic.setAddress(dto.getAddress());
+        clinic.setPhone(dto.getPhone());
+        return clinicRepository.save(clinic);
+    }
+    public void deleteClinicById(int id){
+        Clinic clinic = clinicRepository.findById(id).orElseThrow(() -> new RuntimeException("Clinic not found"));
+        clinicRepository.delete(clinic);
+    }
 }
+
+

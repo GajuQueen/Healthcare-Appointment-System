17689f50b52362cb5e8a0dec17a0bd8c9a0c6075
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-06-09 13:00:50 +0200

    feat: Create PatientDTO for structured API responses

--- src/main/java/com/example/demo/Dto/PatientDTO.java
+++ src/main/java/com/example/demo/Dto/PatientDTO.java
@@ -1,6 +1,5 @@
-package com.example.demo.Patient;
+package com.example.demo.Dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;

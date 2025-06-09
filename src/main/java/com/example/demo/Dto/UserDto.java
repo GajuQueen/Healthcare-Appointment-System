aeed89006b46c7b805a1050977b0ed4c85af58b0
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-06-09 13:00:50 +0200

    feat: Add UserDto to manage authentication payloads

--- src/main/java/com/example/demo/Dto/UserDto.java
+++ src/main/java/com/example/demo/Dto/UserDto.java
@@ -1,4 +1,22 @@
 package com.example.demo.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class UserDto {
+    @NotBlank
+    @Schema(required = true, maxLength = 50)
+    private String firstName;
+    @NotBlank
+    @Schema(required = true, maxLength =50)
+    private String lastName;
+    @NotBlank
+    @Schema(required = true, maxLength = 50)
+    @Column(unique = true)
+    private String email;
+    private String password;
 }
+

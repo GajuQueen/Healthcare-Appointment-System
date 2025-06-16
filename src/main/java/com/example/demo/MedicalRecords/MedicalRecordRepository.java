c28a07ddd2c876fb5b4326440d2559e92fab9a51
GajuQueen <gajuq7@gmail.com>
2025-06-16 10:55:03 +0200

    Refactor MedicalRecordRepository queries

--- src/main/java/com/example/demo/MedicalRecords/MedicalRecordRepository.java
+++ src/main/java/com/example/demo/MedicalRecords/MedicalRecordRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
-    List<MedicalRecord> findByPatientId(long patientId);
-    List<MedicalRecord> findByDoctorId(long doctorId);
-    List<MedicalRecord> findByPatientIdAndDoctorId(long patientId, long doctorId);
+
 }
